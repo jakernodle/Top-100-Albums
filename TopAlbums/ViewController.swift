204a9be6947bb389b4d534de3a48c77fd09a446d
JohnAnge Kernodle <jakernodle@gmail.com>
2022-06-26 15:13:35 -0400

    Update ViewController.swift
    Removed commented code

--- TopAlbums/ViewController.swift
+++ TopAlbums/ViewController.swift
@@ -85,15 +85,6 @@ extension ViewController: UICollectionViewDataSource {
 
 extension ViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {        
-        //let button = UIButton()
-        //button.setTitle("", for: .normal)
-        //let barButton = UIBarButtonItem()
-        //barButton.customView = button
-        //navigationItem.backBarButtonItem = barButton
-        
-        //self.navigationItem.setHidesBackButton(true, animated: true)
-
-        
         let albumDetailVC = AlbumDetailViewController()
         if let albums = App.shared.albums {
             albumDetailVC.albumData = albums[indexPath.item]
